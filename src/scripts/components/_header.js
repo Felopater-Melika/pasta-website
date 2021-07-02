1dc4fbc71f2d14c3f7106067628eec8d4dcebbd7
Felopater <68111601+Felopater2004@users.noreply.github.com>
2021-07-02 17:31:07 -0500

    finished navigation

--- src/scripts/components/_header.js
+++
@@ -1,4 +0,0 @@
-export const menu = document.querySelector('.menu');
-export const cookAtHome = document.querySelector('.cook-at-home');
-export const about = document.querySelector('.about')
-export const news = document.querySelector('.news')
