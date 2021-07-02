044577af57d7817bda90e408d1cfa2cb6ca6ccee
Felopater <68111601+Felopater2004@users.noreply.github.com>
2021-07-01 19:51:54 -0500

    navigation pre-final stage

---
+++ .history/src/scripts/components/_header_20210701194714.js
@@ -0,0 +1,2 @@
+export const menu = document.querySelector('.menu');
+export const cookAtHome = document.querySelector('.cook-at-home');
---
+++ src/scripts/components/_header.js
@@ -0,0 +1,4 @@
+export const menu = document.querySelector('.menu');
+export const cookAtHome = document.querySelector('.cook-at-home');
+export const about = document.querySelector('.about')
+export const news = document.querySelector('.news')
